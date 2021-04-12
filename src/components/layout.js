8ec12dafbea88a9120cec94f268b9149d4111df6
ablache <alexandreblache.pro@gmail.com>
2021-04-12 13:55:14 +0200

    initialisation blog strapi

---
+++ src/components/layout.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
+import Seo from "./seo";
+
+const Layout = ({ children, seo }) => (
+    <StaticQuery
+        query={graphql`
+      query {
+        strapiHomepage {
+          seo {
+            metaTitle
+            metaDescription
+            shareImage {
+              publicURL
+            }
+          }
+        }
+      }
+    `}
+        render={(data) => (
+            <>
+                <Seo seo={seo} />
+                <main>{children}</main>
+            </>
+        )}
+    />
+);
+
+Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
+export default Layout;
