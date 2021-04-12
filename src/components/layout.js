76824cd759748ec8c6f03d975b26e6f2c00cd6a1
ablache <alexandreblache.pro@gmail.com>
2021-04-12 14:40:54 +0200

    articles ok

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
 import Seo from "./seo";
+import Nav from "./nav";
+
 
 const Layout = ({ children, seo }) => (
     <StaticQuery
@@ -21,6 +23,7 @@ const Layout = ({ children, seo }) => (
         render={(data) => (
             <>
                 <Seo seo={seo} />
+                <Nav />
                 <main>{children}</main>
             </>
         )}
